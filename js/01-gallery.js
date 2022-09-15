7a99edf674d835e98b27c5f8028186cb71fcdfc4
StanyslavCherniakov <stas.chernykov@gmail.com>
2022-09-16 01:29:09 +0300

    task-01 DONE

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const imgGalleryRef = document.querySelector(".gallery");
+const galleryMarkUp = createMakrUp(galleryItems);
+imgGalleryRef.innerHTML = galleryMarkUp;
+
+imgGalleryRef.addEventListener("click", onImageClick);
+
+function createMakrUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
+    <img src=${event.target.dataset.source}>
+`
+  );
+  instance.show();
+
+  document.addEventListener("keydown", onEscCloseModal);
+
+  function onEscCloseModal(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", onEscCloseModal);
+    }
+  }
+}
